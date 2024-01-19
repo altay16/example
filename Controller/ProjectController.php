8c2f616cf7700cd800e1463ebeb1ef8784961d21
altay16 <arif.adiguezel@gmail.com>
2024-01-19 13:37:27 +0100

    MVC Controller

---
+++ Controller/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Controller;
+
+class ProjectController {
+
+    public function indexAction() {
+        
+        echo "this is the indexAction";
+        
+    }
+
+    public function detailAction($projectId) {
+        
+        echo "this is the detailAction";
+    }
+
+    public function kontaktAction() {
+        
+        echo "this is the KontaktAction";
+        
+    }
+}
+
--- index.php
+++ index.php
@@ -1,5 +1,49 @@
 <?php
 
-echo "Helloooo";
+require_once 'Model/Model.php';
+require_once 'View/View.php';
+require_once 'Controller/ProjectController.php';
+
+
+
+$requestUri = $_SERVER['REQUEST_URI'];
+
+
+// Routes
+$routes = [
+    '/' => 'ProjectController@indexAction',
+    '/project/(\d+)' => 'ProjectController@detailAction',
+    '/kontakt' =>'ProjectController@kontaktAction'
+];
+
+// Suche nach passender Route
+$matchedRoute = null;
+foreach ($routes as $routePattern => $controllerAction) {
+    $pattern = '~^' . $routePattern . '$~';
+    if (preg_match($pattern, $requestUri, $matches)) {
+        $matchedRoute = $controllerAction;
+        break;
+    }
+}
+
+// Wenn keine passende Route gefunden wurde, zeige eine 404-Seite an
+if (!$matchedRoute) {
+    header('HTTP/1.0 404 Not Found');
+    echo '404 Not Found';
+    exit;
+}
+
+// Aufteilen des Controller-Action-Strings
+list($controllerName, $actionName) = explode('@', $matchedRoute);
+
+// Instanz des Controllers erstellen
+$controller = new $controllerName();
+
+// Die Action-Methode aufrufen
+if (isset($matches[1])) {
+    $controller->$actionName($matches[1]);
+} else {
+    $controller->$actionName();
+}
 
 ?>
